f0f15dd1b13491648aa9e197d2982acd6babbae3
Martin Lyrå <martin.lyra.97@gmail.com>
2019-02-21 15:14:04 +0100

    Updated code from lab session

--- app.py
+++ app.py
@@ -5,29 +5,36 @@ import traceback
 import RPi.GPIO as GPIO
 from project.modules import rfid
 from project.modules.rfid_thread_class import rfid_class
+import logging
+from project.folder import GLOBAL_VARIABLES
 #from project.modules import sensors
 
 
 #GPIO.setmode(GPIO.BCM)
 #GPIO.setup(2, GPIO.IN)
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-9s) %(message)s',
+                    )
 
 def run():
-    #init_rfid()
+    init_rfid()
 
-    test = rfid_class
+    #test = rfid_class()
 
-    test.run(test)
+    #test.run()
 
-    #while True:
-     #   time.sleep(1)
-      #  print("main loop")
-       # pass
+    while True:
+        time.sleep(1)
+        logging.debug("main loop %s", GLOBAL_VARIABLES.alarm_armed)
+        pass
         
 
 def init_rfid():
     try:
-        t = Thread(target=rfid_class, args=(""))
+        #reader = rfid_class()
+        #reader.start()
+        t = rfid_class()
         t.start()
     except:
         print("Error: unable to start thread")
--- modules/rfid_thread_class.py
+++ modules/rfid_thread_class.py
@@ -5,97 +5,123 @@ import RPi.GPIO as GPIO
 import threading
 from project.MFRC522python import SimpleMFRC522
 import pickle
+from datetime import datetime, timedelta
+from project.folder import GLOBAL_VARIABLES
 
 #import SimpleMFRC522
 
 #from squid import *
 #from button import Button
 
-class rfid_class(threading.Thread):   
+class rfid_class(threading.Thread):
+
+    mode = "LISTEN"
+    reader = SimpleMFRC522.SimpleMFRC522()
+    allowed_tags = []
+    recently_read_tags = []
    
     def handle_listen_mode(self):
-        global mode, allowed_tags
-        led.set_color(GREEN)
-        id = reader.read_id_no_block()
+        #led.set_color(GREEN)
+        id = self.reader.read_id_no_block()
         logging.debug("Listen")
+        if id and len(self.recently_read_tags) > 0:
+            for tag in self.recently_read_tags:
+                if id in tag:
+                    return
+            
         if id:
-            if id in allowed_tags:
-                unlock_door()
+            if id in self.allowed_tags:
+                self.recently_read_tags.append((id, datetime.now()))
+                self.unlock_door()
             else:
                 print("Unknown Tag")
-                flash(RED, 5, 0.1)
-        if button.is_pressed():
-            print("pressed")
-            mode = 'GRANT'
+                #flash(RED, 5, 0.1)
+        #if button.is_pressed():
+        #    print("pressed")
+        #    self.change_mode('GRANT')
+
+    
         
     def handle_grant_mode(self):
-        global mode, allowed_tags
-        led.set_color(CYAN)
-        id = reader.read_id_no_block()
-        if id and id not in allowed_tags:
-            allowed_tags.append(id)
-            save_tags()
-            flash(GREEN, 1, 0.1)
-        if button.is_pressed():
-            mode = 'REVOKE'
+        #led.set_color(CYAN)
+        id = self.reader.read_id_no_block()        
+        if id and id not in self.allowed_tags:
+            self.allowed_tags.append(id)
+            self.save_tags()
+            #flash(GREEN, 1, 0.1)
+        #if button.is_pressed():
+        #    self.change_mode('REVOKE')
             
     def handle_revoke_mode(self):
-        global mode
-        led.set_color(PURPLE)
-        id = reader.read_id_no_block()
-        if id and id in allowed_tags:
-            allowed_tags.remove(id)
-            save_tags()
-            flash(PURPLE, 1, 0.1)
-        if button.is_pressed():
-            mode = 'LISTEN'
+        #led.set_color(PURPLE)
+        id = self.reader.read_id_no_block()
+        if id and id in self.allowed_tags:
+            self.allowed_tags.remove(id)
+            self.save_tags()
+            #flash(PURPLE, 1, 0.1)
+        #if button.is_pressed():
+        #    self.change_mode('LISTEN')
             
     def unlock_door(self):
         print("Door UNLOCKED")
-        flash(GREEN, 10, 0.5)
-        print("Door LOCKED")
+        GLOBAL_VARIABLES.alarmed_armed = True
         
-    def flash(self, color, times, delay):
-        for i in range(0, times):
-            led.set_color(color)
-            time.sleep(delay)
-            led.set_color(OFF)
-            time.sleep(delay) 
+    #def flash(self, color, times, delay):
+    #    for i in range(0, times):
+    #        led.set_color(color)
+    #        time.sleep(delay)
+    #        led.set_color(OFF)
+    #        time.sleep(delay) 
             
     def load_tags(self):
-        global allowed_tags
         try:
-            with open('allowed_tags.pickle', 'rb') as handle:
-                allowed_tags = pickle.load(handle)
+            with open('/home/pi/Desktop/project/data/allowed_tags.pickle', 'rb') as handle:
+                self.allowed_tags = pickle.load(handle)
             print("Loaded Tags")
-            print(allowed_tags)      
+            print(self.allowed_tags)      
         except:
             pass
         
     def save_tags(self):
-        global allowed_tags
         print("Saving Tags")
-        print(allowed_tags)
-        with open('allowed_tags.pickle', 'wb') as handle:
-            pickle.dump(allowed_tags, handle)
+        print(self.allowed_tags)
+        with open('/home/pi/Desktop/project/data/allowed_tags.pickle', 'wb') as handle:
+            pickle.dump(self.allowed_tags, handle)
+
+    def check_recent_tags(self):
+        for x in self.recently_read_tags:
+            timestamp = datetime.now()
+
+            dt = timestamp - x[1]
+            delta_ms = dt.microseconds / 1000 + dt.seconds * 1000
+
+            print("Time difference: ", delta_ms)
+
+            if (delta_ms) > 1000:
+                self.recently_read_tags.remove(x)
+                print("Timeout for tag ", x[0])
+
+    def change_mode(self, newMode):
+        self.mode = newMode
+        logging.debug("Mode changed; now ", newMode)
 
     def __init__(self):
+        threading.Thread.__init__(self)
         pass
 
     def run(self):
-        reader = SimpleMFRC522.SimpleMFRC522()
-        mode = 'LISTEN'
-        allowed_tags = []
-        self.load_tags(self)
+        self.load_tags()
         try:
             while True:
-                if mode == "LISTEN":
+                if self.mode == "LISTEN":
                     self.handle_listen_mode()
-                elif mode == "GRANT":
+                elif self.mode == "GRANT":
                     self.handle_grant_mode()
-                elif mode == "REVOKE":
+                elif self.mode == "REVOKE":
                     self.handle_revoke_mode()
+                self.check_recent_tags()
                 sys.stdout.flush()
+                
         finally:
             print("cleaning up")
             GPIO.cleanup()
